00ce11d482f125b2d6e4ad2972864aea4f7dae43
Tamás Batka <batkatom@hotmail.com>
2023-01-31 21:55:43 +0100

    Fetch and display cards

--- greenspark-frontend/src/App.vue
+++ greenspark-frontend/src/App.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
+
+import { API_SERVICE } from "./api/ApiService";
+import { inject, onMounted, ref } from "vue";
+import Widget from "./types/Widget";
+import ProductCard from "./components/ProductCard.vue";
+import ProductList from "./components/ProductList.vue";
+
+const apiService = inject(API_SERVICE);
+const widgets = ref<Widget[] | undefined>([]);
+
+onMounted(async () => {
+  widgets.value = await apiService?.fetchWidgets();
+});
+
 </script>
 
 <template>
-  <div>
+  <div class="container">
+    <ProductList>
+      <ProductCard v-for="widget of widgets" :widget="widget" :key="widget.id"></ProductCard>
+    </ProductList>
   </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+  }
 </style>

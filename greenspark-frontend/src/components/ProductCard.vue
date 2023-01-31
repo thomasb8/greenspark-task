81010f56511a53fdc457960c965d67a7d2222695
Tamás Batka <batkatom@hotmail.com>
2023-01-31 21:55:27 +0100

    Create product card and list with basic design

---
+++ greenspark-frontend/src/components/ProductCard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  import Widget from "../types/Widget";
+  const props = defineProps<{
+    widget: Widget
+  }>();
+
+  function generateActionText() {
+    return `This product ${props.widget.action} ${props.widget.amount} ${props.widget.type}`;
+  }
+
+  function generateProductText() {
+    return `${props.widget.amount} ${props.widget.type}`;
+  }
+</script>
+
+<template>
+  <div class="product-card-container">
+    <div class="title" :class="widget.selectedColor">
+      <div>logo</div>
+      <h3>{{ generateActionText() }}</h3>
+      <h3>{{ generateProductText() }}</h3>
+    </div>
+    <div class="options">
+      <div>{{ props.widget.linked }}</div>
+      <div>{{ props.widget.selectedColor }}</div>
+      <div>{{ props.widget.active }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.product-card-container {
+  .title {
+    &.blue {
+      background: $color-blue;
+    }
+    &.green {
+      background: $color-green;
+    }
+    &.beige {
+      background: $color-beige;
+    }
+  }
+  .options {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
---
+++ greenspark-frontend/src/components/ProductList.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="product-list-container">
+    <h3>Per product widgets</h3>
+    <div class="list">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.product-list-container {
+  background: $color-white;
+  border-radius: 8px;
+  color: $color-black;
+  padding: 2.25rem;
+  h3 {
+    font-size: 1.875rem;
+    font-weight: 700;
+    line-height: 2.25rem;
+    &::after {
+      content: '';
+      display: block;
+      height: 2px;
+      background: $color-border;
+      width: 100%;
+      margin: 0.75rem 0 1.25rem;
+    }
+  }
+  .list {
+    display: flex;
+  }
+}
+</style>

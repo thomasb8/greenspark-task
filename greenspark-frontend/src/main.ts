d521929a007d61af49efbe4bade00959e7ebfa97
Tamás Batka <batkatom@hotmail.com>
2023-02-02 18:10:18 +0100

    Add vuetify for tooltip

--- greenspark-frontend/src/main.ts
+++ greenspark-frontend/src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
-import './style.scss'
 import App from './App.vue'
 import ApiService, { API_SERVICE } from "./api/ApiService";
 
-const app = createApp(App)
+import '@mdi/font/css/materialdesignicons.css'
+import './style.scss'
+import 'vuetify/styles'
+import { createVuetify } from "vuetify";
 
-const apiService = new ApiService(import.meta.env.VITE_API_BASE_URL)
+const app = createApp(App);
 
-app.provide(API_SERVICE, apiService);
+const apiService = new ApiService(import.meta.env.VITE_API_BASE_URL);
+
+
+app.use(createVuetify()).provide(API_SERVICE, apiService);
 
 app.mount('#app')

843d6c6b842e735bc9394a71ed18a1f573a86ad7
Tamás Batka <batkatom@hotmail.com>
2023-02-03 00:18:16 +0100

    Allow cors origins

--- greenspark-backend/src/main.ts
+++ greenspark-backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from "@nestjs/common";
+import { NotFoundException, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: (origin, cb) => {
+        if (origin && !JSON.parse(process.env.CORS_ORIGINS || '[]').includes(origin)) {
+          return cb(new NotFoundException());
+        }
+        cb(null, true);
+      },
+      credentials: true
+    }
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
